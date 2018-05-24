27bcbaac438494162e72ae76c5776a110f97e949
clain6004 <kirisame6004@gmail.com>
2018-05-25 08:04:42 +0900

    青志祭ランゲー　緊急コミット やばいやばい

--- Assets/cs.cs
+++ Assets/cs.cs
@@ -9,28 +9,113 @@ public class cs : MonoBehaviour {
 
     private Vector3 moveD=Vector3.zero;
 
+    CharacterController a;
+
+    public float sliit = 1f;
+
+    Animator ani;
+
+    Vector3 collc;
+
+    float collh;
+
+    float slit = 0f;
+
 
 	// Use this for initialization
 	void Start () {
 		
+        a=GetComponent<CharacterController>();
+
+        collc = a.center;
+
+        collh = a.height;
+
+        ani = GetComponent<Animator>();
+
+
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-        CharacterController controller=GetComponent<CharacterController>();
-        if(controller.isGrounded){
 
-            moveD=new Vector3(Input.GetAxis("Horizontal"),0,0);
+        bool isjump = Input.GetKey(KeyCode.Space);
+
+        bool isslli = Input.GetKey(KeyCode.DownArrow);
+
+        ani.SetBool("IsGround",a.isGrounded);
+
+        if (slit > 0)
+        {
+
+            slit -= Time.deltaTime;
+
+        }
+
+        ani.SetFloat("slif", slit);
+
+        AnimatorStateInfo state = ani.GetCurrentAnimatorStateInfo(0);
+
+        if(state.nameHash==Animator.StringToHash("base Layer.sli")) {
+
+            Vector3 newa = collc;
+
+            newa.y *= 0.5f;
+
+            a.center = newa;
+
+            a.height = collh * 0.5f;
+
+        }
+        else
+        {
+
+            a.center = collc;
+
+            a.height = collh;
+
+        }
+
+
+
+        if(a.isGrounded){
+
+            moveD =new Vector3(Input.GetAxis("Horizontal"),0,0);
+
 moveD=transform.TransformDirection(moveD);
-            moveD*=s;
-            if(Input.GetButton("Jump"))
-            moveD.y=jumped;
+
+            moveD *=s;
+
+            if (isjump)
+            {
+
+                ani.SetTrigger("JumpStart");
 
             }
 
+            if (isslli && slit <= 0)
+            {
+
+                ani.SetTrigger("sli");
+
+                slit = sliit;
+
+                ani.SetFloat("slif", slit);
+
+            }
+
+            if (Input.GetButton("Jump"))
+            {
+
+                moveD.y = jumped;
+
+            }
+            }
+
         moveD.y-=gr*Time.deltaTime;
-        controller.Move(moveD*Time.deltaTime);
+
+        a.Move(moveD*Time.deltaTime);
 
 }
 
